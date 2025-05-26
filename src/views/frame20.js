ce13e63cf6812b9e55c9ed41a0db613e4948d5c6
samukako <samuelkleinoliveira@gmail.com>
2025-05-26 18:23:56 -0300

    Commit from TeleportHQ

---
+++ src/views/frame20.js
@@ -0,0 +1,237 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './frame20.css'
+
+const Frame20 = (props) => {
+  return (
+    <div className="frame20-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="frame20-frame20">
+        <div className="frame20-group2">
+          <div className="frame20-frame161">
+            <span className="frame20-text10">Ordens de serviço no período</span>
+            <div className="frame20-frame191">
+              <div className="frame20-frame171">
+                <img
+                  alt="Ellipse12007"
+                  src="/external/ellipse12007-y9me-200h.png"
+                  className="frame20-ellipse11"
+                />
+                <span className="frame20-text11">O.S. no período</span>
+              </div>
+              <div className="frame20-frame181">
+                <img
+                  alt="Ellipse12007"
+                  src="/external/ellipse12007-g8f5-200h.png"
+                  className="frame20-ellipse12"
+                />
+                <span className="frame20-text12">O.S. no período anterior</span>
+              </div>
+            </div>
+          </div>
+          <div className="frame20-group1">
+            <div className="frame20-frame1">
+              <div className="frame20-chart">
+                <div className="frame20-group10">
+                  <img
+                    alt="Vector2007"
+                    src="/external/vector2007-ayodn.svg"
+                    className="frame20-vector10"
+                  />
+                  <div className="frame20-group11">
+                    <div className="frame20-group12">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-6asa.svg"
+                        className="frame20-vector11"
+                      />
+                    </div>
+                    <div className="frame20-group13">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-hz29.svg"
+                        className="frame20-vector12"
+                      />
+                      <span className="frame20-text13">25</span>
+                    </div>
+                    <div className="frame20-group14">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-8pl6.svg"
+                        className="frame20-vector13"
+                      />
+                      <span className="frame20-text14">50</span>
+                    </div>
+                    <div className="frame20-group15">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-8kfk.svg"
+                        className="frame20-vector14"
+                      />
+                      <span className="frame20-text15">75</span>
+                    </div>
+                    <div className="frame20-group16">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-mbt.svg"
+                        className="frame20-vector15"
+                      />
+                      <span className="frame20-text16">100</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="frame20-group17">
+                  <img
+                    alt="Vector2007"
+                    src="/external/vector2007-4q46.svg"
+                    className="frame20-vector16"
+                  />
+                </div>
+                <div className="frame20-group18">
+                  <img
+                    alt="Vector2007"
+                    src="/external/vector2007-sfs8.svg"
+                    className="frame20-vector17"
+                  />
+                  <div className="frame20-group19">
+                    <div className="frame20-group20">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-m4c.svg"
+                        className="frame20-vector18"
+                      />
+                    </div>
+                    <div className="frame20-group21">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-eihs.svg"
+                        className="frame20-vector19"
+                      />
+                      <span className="frame20-text17">01/04</span>
+                    </div>
+                    <div className="frame20-group22">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-scl5.svg"
+                        className="frame20-vector20"
+                      />
+                      <span className="frame20-text18">03/04</span>
+                    </div>
+                    <div className="frame20-group23">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-4b5l.svg"
+                        className="frame20-vector21"
+                      />
+                      <span className="frame20-text19">05/04</span>
+                    </div>
+                    <div className="frame20-group24">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-gux.svg"
+                        className="frame20-vector22"
+                      />
+                      <span className="frame20-text20">07/04</span>
+                    </div>
+                    <div className="frame20-group25">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-er7n.svg"
+                        className="frame20-vector23"
+                      />
+                      <span className="frame20-text21">09/04</span>
+                    </div>
+                    <div className="frame20-group26">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-5igq.svg"
+                        className="frame20-vector24"
+                      />
+                      <span className="frame20-text22">11/04</span>
+                    </div>
+                    <div className="frame20-group27">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-h0huo.svg"
+                        className="frame20-vector25"
+                      />
+                      <span className="frame20-text23">13/04</span>
+                    </div>
+                    <div className="frame20-group28">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-tqxu.svg"
+                        className="frame20-vector26"
+                      />
+                      <span className="frame20-text24">15/04</span>
+                    </div>
+                    <div className="frame20-group29">
+                      <img
+                        alt="Vector2007"
+                        src="/external/vector2007-6wco.svg"
+                        className="frame20-vector27"
+                      />
+                      <span className="frame20-text25">17/04</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="frame20-group30">
+                  <img
+                    alt="Vector2007"
+                    src="/external/vector2007-mz1.svg"
+                    className="frame20-vector28"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame20-group3">
+          <div className="frame20-frame162">
+            <span className="frame20-text26">Ordens de serviço por tipo</span>
+            <div className="frame20-frame192">
+              <div className="frame20-frame172">
+                <img
+                  alt="Ellipse12007"
+                  src="/external/ellipse12007-vij2-200h.png"
+                  className="frame20-ellipse13"
+                />
+                <span className="frame20-text27">Manutenção</span>
+              </div>
+              <div className="frame20-frame182">
+                <img
+                  alt="Ellipse12007"
+                  src="/external/ellipse12007-l7de-200h.png"
+                  className="frame20-ellipse14"
+                />
+                <span className="frame20-text28">Instalação</span>
+              </div>
+            </div>
+            <div className="frame20-group31">
+              <div className="frame20-group32">
+                <img
+                  alt="Vector2007"
+                  src="/external/vector2007-5zz9.svg"
+                  className="frame20-vector29"
+                />
+              </div>
+              <div className="frame20-group33">
+                <img
+                  alt="Vector2007"
+                  src="/external/vector2007-9r6q.svg"
+                  className="frame20-vector30"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Frame20
